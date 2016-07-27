064c75801a2542e8a03e366f9f6b79527eead35d
Christian Hegedues <github@christianhegedues.de>
2016-07-27 23:07:39 +0200

    check for files before moving them

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,21 +10,39 @@ dir=$PWD # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
 files="bashrc bash_profile bash_prompt vimrc vim" # list of files/folders to symlink in homedir
 
-##########
-
+#
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+#
+echo "Creating $olddir for backup of any existing dotfiles in ~/ ..."
 mkdir -p $olddir
-echo "done"
+echo "... done"
+echo ""
 
+#
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+#
+echo "Changing to the $dir directory ..."
 cd $dir
-echo "done"
+echo "... done"
+echo ""
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+#
+# move any existing dotfiles in homedir to dotfiles_old directory
+#
 for file in $files; do
-mv ~/.$file ~/dotfiles_old/
-echo "Creating symlink to $file in home directory."
-ln -s $dir/.$file ~/.$file
+  if [ -a ~/.$file ]; then
+    mv ~/.$file ~/dotfiles_old/
+  fi
 done
+
+#
+# create symlinks from the homedir to any files in the ~/dotfiles directory
+# specified in $files
+#
+echo "Creating symlinks in home directory"
+for file in $files; do
+  echo "~/.$file -> $dir/.$file"
+  ln -s $dir/.$file ~/.$file
+done
+echo "... done"
+echo ""
