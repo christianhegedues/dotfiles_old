c66614cf1249b69b3edbe11be88a1fdaebead2cd
Christian Hegedüs <github@christianhegedues.de>
2016-07-30 14:33:19 +0200

    .inputrc: initial commit

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=$PWD # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="bashrc bash_profile bash_prompt aliases vimrc vim" # list of files/folders to symlink in homedir
+files="bashrc bash_profile bash_prompt aliases inputrc vimrc vim" # list of files/folders to symlink in homedir
 
 #
 # create dotfiles_old in homedir
