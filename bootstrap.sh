c1b0aa25746f8f0171ed1341bb4f237b801fdd3a
Christian Hegedüs <github@christianhegedues.de>
2016-08-23 15:10:07 +0200

    include bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# A simple script for setting up OSX dev environment.
+
+dev="$HOME/Developer"
+pushd .
+mkdir -p $dev
+cd $dev
+
+echo 'Enter new hostname of the machine (e.g. macbook-paulmillr)'
+read hostname
+echo "Setting new hostname to $hostname..."
+scutil --set HostName "$hostname"
+compname=$(sudo scutil --get HostName | tr '-' '.')
+echo "Setting computer name to $compname"
+scutil --set ComputerName "$compname"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
+
+pub=$HOME/.ssh/id_rsa.pub
+echo 'Checking for SSH key, generating one if it does not exist...'
+[[ -f $pub ]] || ssh-keygen -t rsa
+
+echo 'Copying public key to clipboard. Paste it into your Github account...'
+[[ -f $pub ]] && cat $pub | pbcopy
+open 'https://github.com/account/ssh'
+
+# If we on OS X, install homebrew and tweak system a bit.
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+    brew install htop mysql nginx node ruby
+  fi
+
+  # http://github.com/sindresorhus/quick-look-plugins
+  echo 'Installing Quick Look plugins...'
+  brew tap phinze/homebrew-cask
+  brew install caskroom/cask/brew-cask
+  brew cask install suspicious-package quicklook-json qlmarkdown qlstephen qlcolorcode
+fi
+
+# echo 'Symlinking config files...'
+# source 'bin/symlink-dotfiles.sh'
+
+popd
